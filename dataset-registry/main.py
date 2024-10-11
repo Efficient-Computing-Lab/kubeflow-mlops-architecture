b6b7b3a94d0ef06776b22be1c43168f43f7b1ef6
giannis <gkorod@hua.gr>
2024-10-11 18:31:21 +0300

    update of topo_example.py

--- dataset-registry/main.py
+++ dataset-registry/main.py
@@ -1,11 +1,12 @@
 from flask import Flask, send_file, abort, request
 from werkzeug.utils import safe_join
 import os
+import logging
 import zipfile
 
 app = Flask(__name__)
 
-directory = os.environ.get('ENV_PATH', 'data')
+directory = os.environ.get('ENV_PATH', '/home/giannis')
 
 
 # Option 1: Using send_file
@@ -35,14 +36,19 @@ def upload_file():
     # Check if the file is empty
     if file.filename == '':
         return 'No selected file', 400
-
+        # Get model_name from the request form data
+    model_name = request.form.get('model_name')
+    if not model_name:
+        return 'No model_name provided', 400
+    models_path = directory+"/"+model_name
+    if not os.path.exists(models_path):
+        os.makedirs(models_path)
     # Save the file to a local directory with its original filename
-    file.save(os.path.join(directory, file.filename))
-    # Check if the uploaded file is a zip file
-    if zipfile.is_zipfile(directory):
-        # Unzip the file into the same directory
-        with zipfile.ZipFile(directory, 'r') as zip_ref:
-            zip_ref.extractall(directory)
+    file.save(os.path.join(models_path, file.filename))
+    zip_file_path = models_path +"/"+ file.filename
+    if zipfile.is_zipfile(zip_file_path):
+        with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+            zip_ref.extractall(models_path)
     return 'File uploaded successfully', 200
 
 
--- pipeline_dsl_example/topo_example.py
+++ pipeline_dsl_example/topo_example.py
@@ -8,6 +8,86 @@ import tarfile
 import os
 import kfp
 
+@component(
+    base_image="gkorod/topo:1.0",
+)
+def configure_parameters():
+    import subprocess
+    import shutil
+    import os
+    import yaml
+    command="rm -r /datasets/params.yml"
+    subprocess.run(command, shell=True)
+    new_params_yaml ={
+      "dataset": "carObj12",
+      "model_variant": "xception_65",
+      "atrous_rates": [12, 24, 36],
+      "encoder_output_stride": 8,
+      "decoder_output_stride": [4],
+      "upsample_logits": False,
+      "frag_seg_agnostic": False,
+      "frag_loc_agnostic": False,
+      "num_frags": 64,
+      "corr_min_obj_conf": 0.1,
+      "corr_min_frag_rel_conf": 0.5,
+      "corr_project_to_model": False,
+      "train_tfrecord_names": ["carObj12_train-primesense"],
+      "train_max_height_before_crop": 480,
+      "train_crop_size": "640, 480",
+      "freeze_regex_list": ["xception_65/entry_flow"],
+      "initialize_last_layer": True,
+      "fine_tune_batch_norm": False,
+      "train_steps": 15480,
+      "train_batch_size": 1,
+      "base_learning_rate": 0.0000001,
+      "learning_power": 0.9,
+      "obj_cls_loss_weight": 1.0,
+      "frag_cls_loss_weight": 1.0,
+      "frag_loc_loss_weight": 100.0,
+      "train_knn_frags": 1,
+      "data_augmentations": {
+        "random_adjust_brightness": {
+          "min_delta": -0.2,
+          "max_delta": 0.4
+        },
+        "random_adjust_contrast": {
+          "min_delta": 0.6,
+          "max_delta": 1.4
+        },
+        "random_adjust_saturation": {
+          "min_delta": 0.6,
+          "max_delta": 1.4
+        },
+        "random_adjust_hue": {
+          "max_delta": 1.0
+        },
+        "random_blur": {
+          "max_sigma": 1.5
+        },
+        "random_gaussian_noise": {
+          "max_sigma": 0.1
+        }
+      },
+      "eval_tfrecord_names": ["tless_test_targets-bop19"],
+      "eval_max_height_before_crop": 480,
+      "eval_crop_size": "640, 480",
+      "infer_tfrecord_names": ["carObj12_test"],
+      "infer_max_height_before_crop": 480,
+      "infer_crop_size": "640, 480"
+    }
+    # Save the dictionary to a YAML file
+    with open('/app/store/tf_models/obj12/params.yml', 'w') as file:
+        yaml.dump(new_params_yaml, file, default_flow_style=False)
+    # Define source and destination paths
+    training_set_source = "/datasets/epos/training_set"
+    training_set_destination = "/app/datasets/carObj12/train_primesense"
+
+    # Create the destination directory if it doesn't exist
+    os.makedirs(os.path.dirname(training_set_destination), exist_ok=True)
+
+    # Move the directory
+    shutil.copytree(training_set_source, training_set_destination)
+
 
 # Define the prepare_data component
 @component(
@@ -57,16 +137,19 @@ def train():
     shutil.copytree(src, dst,dirs_exist_ok=True)
 # Define the pipeline
 @pipeline
-def my_pipeline():
+def epos_pipeline():
     """My ML pipeline."""
+    setup_epos = configure_parameters()
+    kubernetes.mount_pvc(setup_epos, pvc_name="datasets", mount_path="/datasets")
     training_task = train()
     training_task.set_accelerator_type('nvidia.com/gpu')
     kubernetes.add_pod_annotation(training_task, 'runtimeClassName', 'nvidia')
     kubernetes.mount_pvc(training_task, pvc_name="trained-models", mount_path="/trained_models")
+    kubernetes.mount_pvc(training_task, pvc_name="datasets", mount_path="/datasets")
 
 
 # Compile the pipeline
-compiler.Compiler().compile(my_pipeline, package_path="pipeline.yaml")
+compiler.Compiler().compile(epos_pipeline, package_path="pipeline.yaml")
 
 # Package the pipeline YAML
 with tarfile.open("pipeline.tar.gz", "w:gz") as tar:
@@ -86,4 +169,5 @@ files = {
 }
 
 # Send the POST request to submit pipeline and initialize training phase
-response = requests.post('http://192.168.1.240:5005/submit', files=files, data={'json_data': json.dumps(json_info)})
+# The following IP is belonging to Erebor
+response = requests.post('http://147.102.109.92:5005/submit', files=files, data={'json_data': json.dumps(json_info)})

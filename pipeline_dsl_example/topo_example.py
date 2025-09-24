f35bdd08ea1835b3ce88c6f6cce02a27a4e33b4b
korontanis <gkorod2@gmail.com>
2025-09-24 13:59:39 +0300

    updated workflow

--- pipeline_dsl_example/topo_example.py
+++ pipeline_dsl_example/topo_example.py
@@ -99,6 +99,8 @@ def setup_train():
     #subprocess.run("ls /datasets/epos", shell=True)
     os.makedirs("/app/datasets/carObj12/train_primesense",exist_ok=True)
     subprocess.run("ls /app/datasets/carObj12",shell=True)
+    subprocess.run("ls /app/datasets/epos/training_training_epos_Objs12", shell=True)
+    subprocess.run("ls /app/datasets/epos/training_training_epos_Objs12/training_set", shell=True)
     subprocess.run("cp -r /datasets/epos/training_training_epos_Objs12/training_set /app/datasets/carObj12/train_primesense",shell=True)
     subprocess.run("ls /app/datasets/carObj12/train_primesense/",shell=True)
     #subprocess.run("ls /app/datasets/carObj12/train_primesense",shell=True)
